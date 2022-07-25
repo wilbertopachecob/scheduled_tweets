d8ad6ea3f673bcfb7c7874b86bf6d74f1ce8a837
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2022-07-25 16:55:53 -0500

    Refactored to use current user

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,9 +2,5 @@ class MainController < ApplicationController
     def index
         # flash.now[:notice]="My first flash" 
         # flash.now[:alert]="My second flash"
-        
-        if session[:user_id]
-            @user = User.find_by(id: session[:user_id])
-        end 
     end    
 end
