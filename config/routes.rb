a453657e4df2ff354449c8b40d3d8b85cc3463da
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2022-07-25 14:59:03 -0500

    Added logout option

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get "about", to: "about#index", as: :about
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
 end
