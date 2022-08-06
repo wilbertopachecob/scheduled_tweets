9fe2182bb80f2e0c43165b206f0ac3e56daeb21e
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2022-08-06 14:45:08 -0500

    added config for mailer to work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,4 +70,6 @@ Rails.application.configure do
   logger           = ActiveSupport::Logger.new(STDOUT)
   logger.formatter = config.log_formatter
   config.logger = ActiveSupport::TaggedLogging.new(logger)
+
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
 end
