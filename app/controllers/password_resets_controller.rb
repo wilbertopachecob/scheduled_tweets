ee4e66d96322a795f1739661d8582e921bbea5e3
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2022-08-06 14:44:55 -0500

    fixed error with User.find. Added view to display password reset edit page

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,12 +4,20 @@ class PasswordResetsController < ApplicationController
     end
 
     def create
-     @user = User.find(email: params[:email])
+     @user = User.find_by(email: params[:email])
      
      if @user.present?
         # send email       
         PasswordMailer.with(user: @user).reset.deliver_later 
      end
      redirect_to root_path, notice: "If a user with that email was found, we have sent an email with a link to reset your password"
+    end
+
+    def edit
+        
+    end
+    
+    def update
+        
     end
 end
