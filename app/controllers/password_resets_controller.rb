1d85818de21eac0d5b69810419b989cec1d32d40
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2022-09-05 12:28:26 -0500

    completed code for edit and update actions

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -14,10 +14,23 @@ class PasswordResetsController < ApplicationController
     end
 
     def edit
-        
+        @user = User.find_signed!(params[:token], purpose: 'password_reset')
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+        redirect_to sign_in_path, alert: "Yor token has expired, please try again"
+    # binding.irb
     end
     
     def update
-        
+        @user = User.find_signed!(params[:token], purpose: 'password_reset')
+        if @user.update(password_params)
+            redirect_to sign_in_path, notice: "Your password was reset correctly. Please sign in"
+        else
+            render :edit, status: 422    
+        end
+    end
+
+    private
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
     end
 end
